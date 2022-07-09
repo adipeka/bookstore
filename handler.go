8744aafc515623b1b8cbc6b97515679a5ac532bc
adipeka <dipekatech@gmail.com>
2022-07-09 11:35:41 +0530

    Added handler

--- handler.go
+++ handler.go
@@ -1,1 +1,24 @@
-package main
+package handler
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type handler struct {
+	db     *sql.DB
+	router *mux.Router
+}
+
+func New(db *sql.DB, r *mux.Router) handler {
+	return handler{
+		db:     db,
+		router: r,
+	}
+}
+
+func (h *handler) GetBook(w http.ResponseWriter, r *http.Request) {
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/gorilla/mux"
+	"github.com/adipeka/bookstore/handler"
+)
+
+const (
+	dbhost     = "localhost"
+	dbport     = "5432"
+	dbuser     = "postgres"
+	dbpassword = "12345"
+	dbname     = "postgres"
+)
+
 func main() {
+
+	connectionstring := fmt.Sprintf("user=%s dbname=%s port=%s password=%s sslmode=disable", dbuser, dbname, dbport, dbpassword)
+	db, err := sql.Open("postgres", connectionstring)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router := mux.NewRouter()
+
+	hr := handler.New(db,router)
 
 }
